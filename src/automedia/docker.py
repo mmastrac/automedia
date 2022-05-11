db1727087e59b8c13fae354eaf252c71cc3de1a1
Matt Mastracci <matthew@mastracci.com>
2022-05-11 13:48:08 -0600

    Also need to support relative roots

--- src/automedia/docker.py
+++ src/automedia/docker.py
@@ -3,7 +3,7 @@ from pathlib import Path
 class Docker:
     def __init__(self, pwd, prefixes) -> None:
         if pwd == None or prefixes == None:
-            self.docker_prefix = Path('/')
+            self.docker_prefix = None
             return
 
         # Test each candidate to see if we can find the pwd
@@ -12,6 +12,7 @@ class Docker:
         for candidate in prefixes:
             if (candidate / pwd.relative_to('/')).exists():
                 self.docker_prefix = candidate
+                self.pwd = candidate / pwd.relative_to('/')
                 return
 
         raise BaseException("Unable to locate docker root")
@@ -20,4 +21,9 @@ class Docker:
         return Docker(None, None)
 
     def dockerize_path(self, path):
-        return self.docker_prefix / Path(path).relative_to('/')
+        path = Path(path)
+        if self.docker_prefix is None:
+            return path
+        if path.is_absolute():
+            return self.docker_prefix / Path(path).relative_to('/')
+        return self.pwd / path
