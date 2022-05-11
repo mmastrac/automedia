10ad1f6937261da0875ac1f81db22be35f937647
Matt Mastracci <matthew@mastracci.com>
2022-05-11 11:24:36 -0600

    Test cleanup

--- tests/test_transcode.py
+++ tests/test_transcode.py
@@ -1,5 +1,4 @@
 from automedia import main
-import unittest
 import pytest
 
 @pytest.mark.parametrize("dir", [1])
@@ -7,4 +6,3 @@ def test_transcode_good(tmp_path, dir):
     print(dir)
     result = main.do_main(['', '--symlinks', '--root', f'tests/transcode-test-{dir}', 'transcode', '--preset', 'aac-64k', '--output', str(tmp_path)])
     assert result == 0
-    pass
--- tests/test_verify.py
+++ tests/test_verify.py
@@ -1,12 +1,10 @@
 from automedia import main
-import unittest
 import pytest
 
 @pytest.mark.parametrize("dir", [1, 2])
 def test_verify_good(dir):
     result = main.do_main(['', '--symlinks', '--root', f'tests/verify-test-{dir}', 'verify'])
     assert result == 0
-
 
 @pytest.mark.parametrize("dir", [1, 2, 3])
 def test_verify_bad(dir):
